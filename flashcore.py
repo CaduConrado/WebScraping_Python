45485f08f9ea60cbbb4d3f766f9c2a8120c13cc0
CaduConrado <caducngo@gmail.com>
2023-05-05 20:39:50 -0300

    Corrigindo endereço

--- flashcore.py
+++ flashcore.py
@@ -1,8 +1,11 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-import time 
 
-driver = webdriver.Firefox()
+import time
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
 
-driver.get("https://flashscore.com.br")
+driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+
+driver.get('https://www.flashscore.com.br')
